6e59fea4a7e66d16465d023a47eea97f1044a21b
alepadel <alepadel@gmail.com>
2021-01-22 19:18:06 +0000

    preprocess functions

--- trainer/task.py
+++ trainer/task.py
@@ -8,6 +8,7 @@ from tensorflow.keras import activations
 from tensorflow.keras import optimizers
 from tensorflow.keras import losses
 from tensorflow.keras import metrics
+from tensorflow.keras.utils import to_categorical
 
 
 def _download_data():
@@ -16,11 +17,17 @@ def _download_data():
     x_test, y_test = test
     return x_train, y_train, x_test, y_test
 
+def _preprocess_data(x,y):
+    x = x / 255.0 
+    y = utils.to_categorical(y)
 
 def train_and_evaluate(batch_size, epochs, job_dir, output_path):
 
     # Download the data
     x_train, y_train, x_test, y_test = _download_data()
+
+    x_train, y_train = _preprocess_data(x_train,y_train)
+    x_test, y_test = _preprocess_data(x_test,y_test)
 
     # Preprocess the data
 
