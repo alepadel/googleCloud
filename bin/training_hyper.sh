0290afbe7ca169261ddc56860567a7cdaacf6e38
alepadel <alepadel@gmail.com>
2021-01-30 08:55:37 +0000

    training.sh created

---
+++ bin/training_hyper.sh
@@ -0,0 +1,14 @@
+
+
+gcloud ai-platform jobs submit training mnist_`date +"%s"` \
+--python-version 3.7 \
+--runtime-version 2.3 \
+--scale-tier BASIC \
+--package-path ./trainer \
+--module-name trainer.task \
+--region europe-west1 \
+--job-dir gs://alepadel-20200122-kschool/tmp \
+--config ./bin/hyper.yaml \
+-- \
+--model-output-path gs://alepadel-20200122-kschool/models \
+--hypertune
