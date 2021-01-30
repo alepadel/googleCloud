4e577357eba10d9c76def8018062fc7c503a7b5c
alepadel <alepadel@gmail.com>
2021-01-30 10:15:46 +0000

    hyper tuninig types of neural networks

--- bin/training.sh
+++ bin/training.sh
@@ -2,6 +2,7 @@
 
 EPOCHS=30
 BATCH_SIZE=1024
+TYPE_OF_MODEL = 'NeuralNetworks' # or ConvolutionalNN
 
 gcloud ai-platform jobs submit training mnist_`date +"%s"` \
 --python-version 3.7 \
@@ -15,3 +16,4 @@ gcloud ai-platform jobs submit training mnist_`date +"%s"` \
 --epochs $EPOCHS \
 --batch-size $BATCH_SIZE \
 --model-output-path gs://alepadel-20200122-kschool/models
+--type-model $TYPE_OF_MODEL
--- trainer/task.py
+++ trainer/task.py
@@ -13,7 +13,9 @@ from tensorflow.keras import losses
 from tensorflow.keras import metrics
 from tensorflow.keras import utils
 from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential, Model
 from tensorflow.keras import callbacks
+from keras.layers import Dense, Dropout, Activation, Flatten, Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from . import __version__
 
 LOGGER = logging.getLogger()
@@ -26,9 +28,12 @@ def _download_data():
     x_test, y_test = test
     return x_train, y_train, x_test, y_test
 
-def _preprocess_data(x,y):
+def _preprocess_data(x,y,type_of_model):
+    
     x = x / 255.0 
     y = utils.to_categorical(y)
+    if type_of_model == 'ConvolutionalNN':
+        x = x.reshape(-1, 28, 28,1)
     return x,y
 
 def _build_model():
@@ -48,18 +53,50 @@ def _build_model():
 
     return m
 
-def train_and_evaluate(batch_size, epochs, job_dir, output_path, is_hypertune):
+def _build_model_CNN(input_shape):
+    input_img = Input(shape=input_shape)  
+    x = Conv2D(16, (5, 5), activation='relu', padding='same')(input_img)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(16, (5, 5), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(16, (5, 5), activation='relu', padding='same')(x)
+    x = Dropout(0.3)(x)
+    y = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    y = MaxPooling2D((2, 2), padding='same')(y)
+    y = Conv2D(16, (3, 3), activation='relu', padding='same')(y)
+    y = MaxPooling2D((2, 2), padding='same')(y)
+    y = Conv2D(16, (3, 3), activation='relu', padding='same')(y)
+    y = Dropout(0.4)(y)
+    z = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    z = MaxPooling2D((2, 2), padding='same')(z)
+    z = Conv2D(16, (3, 3), activation='relu', padding='same')(z)
+    z = MaxPooling2D((2, 2), padding='same')(z)
+    z = Conv2D(16, (3, 3), activation='relu', padding='same')(z)
+
+    out = layers.concatenate([x, y, z])
+    out = layers.Flatten()(out)
+    out = Dense(16, activation='selu')(out)
+    out = Dense(10, activation='softmax')(out)
+    
+    model_f = Model(inputs=[input_img], outputs=[out])
+    return model_f
+
+def train_and_evaluate(batch_size, epochs, job_dir, output_path, is_hypertune,type_of_model):
 
     # Download the data
     x_train, y_train, x_test, y_test = _download_data()
 
     # Preprocess the data
-    x_train, y_train = _preprocess_data(x_train,y_train)
-    x_test, y_test = _preprocess_data(x_test,y_test)
+    x_train, y_train = _preprocess_data(x_train,y_train,type_of_model)
+    x_test, y_test = _preprocess_data(x_test,y_test,type_of_model)
 
     # Build the model
+    if type_of_model == 'ConvolutionalNN':
+        input_shape = [28,28,1]
+        m = _build_model_CNN(input_shape)
+    else:
+        m = _build_model()
 
-    m = _build_model()
     m.compile(loss = losses.categorical_crossentropy, optimizer=optimizers.Adam(),metrics=[metrics.categorical_accuracy])
 
     # Train the model
@@ -92,18 +129,20 @@ def main():
     parser.add_argument('--hypertune', action='store_true', help = 'This is a hypertuning job')
     parser.add_argument('--batch-size', type=int, help='Batch size for the training')
     parser.add_argument('--epochs', type=int, help='Number of epochs for the training')
+    parser.add_argument('--type-model',  help='Choose between regular NN or CNN')
     parser.add_argument('--job-dir', default=None, required=False, help='Option for AI Platform') # opcion obligatoria
     parser.add_argument('--model-output-path', help='Path to write the SaveModel format')
 
     args = parser.parse_args()
 
     is_hypertune = args.hypertune
+    type_of_model = args.type_model
     batch_size = args.batch_size
     epochs = args.epochs
     job_dir = args.job_dir
     output_path = args.model_output_path
 
-    train_and_evaluate(batch_size, epochs, job_dir, output_path, is_hypertune)
+    train_and_evaluate(batch_size, epochs, job_dir, output_path, is_hypertune,type_of_model)
 
 if __name__ == "__main__":
     main()
